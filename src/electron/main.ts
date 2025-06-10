e583d8f3bceec2e36e362efca502c490df5b0c4e
teklu abayneh <tekluabayneh@gmail.com>
2025-06-10 23:52:40 +0300

    inittial commit

---
+++ src/electron/main.ts
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from "electron"
+import path from "path"
+import { isDev } from "./util.js"
+
+// type test = string
+
+app.on("ready", () => {
+    const mainWindow = new BrowserWindow({})
+
+    if (isDev()) {
+        mainWindow.loadURL("https://localhost:5123")
+    } else {
+
+        mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"))
+    }
+
+    mainWindow.webContents.openDevTools();
+})
+
+
+
+
+
+
+
+
+
+
+
