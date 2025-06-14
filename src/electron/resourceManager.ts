b4920c2d2f1691ad6d5b9766cafee709ba0e2a66
teklu abayneh <tekluabayneh@gmail.com>
2025-06-14 21:42:40 +0300

    Add resourceManager to handle Electron resources

---
+++ src/electron/resourceManager.ts
@@ -0,0 +1,54 @@
+import os from "os"
+
+import OsUtils from "os-utils"
+import fs from "fs"
+import { toASCII } from "punycode"
+const POLLING_INTERVAl = 500
+
+export const pollResources = () => {
+    setInterval(async () => {
+        const cpuUsage = await getCpuUsage()
+        const ramUsage = getRamUsage()
+        const getStorage = getStorageData()
+        console.log(cpuUsage)
+        console.log("ram", ramUsage)
+        console.log("starge", getStorage)
+    }, POLLING_INTERVAl)
+
+}
+
+const getCpuUsage = () => {
+    return new Promise((res) => {
+        OsUtils.cpuUsage(res)
+    })
+}
+
+
+const getRamUsage = () => {
+    return 1 - OsUtils.freememPercentage()
+}
+
+
+export const getStaticData = () => {
+    const totalStorage = getStorageData().total
+    const cpuModel = os.cpus()[0].model
+    const totalMemoryGB = Math.floor(OsUtils.totalmem() / 1024)
+
+    return {
+        totalStorage,
+        cpuModel,
+        totalMemoryGB
+    }
+
+}
+
+const getStorageData = () => {
+    const stats = fs.statfsSync(process.platform === "win32" ? "C://" : "/")
+    const total = stats.bsize * stats.blocks
+    const free = stats.bfree * stats.bfree
+
+    return {
+        total: Math.floor(total / 1_000_000_000),
+        useage: 1 - free / total
+    }
+}
