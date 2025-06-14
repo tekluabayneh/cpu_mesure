fda91c7ff6529239fe1132694a243f2febfba88b
teklu abayneh <tekluabayneh@gmail.com>
2025-06-14 21:42:40 +0300

    Update Electron main process logic in main.ts

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,28 +1,21 @@
-import { app, BrowserWindow } from "electron"
-import path from "path"
-import { isDev } from "./util.js"
-
-
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { isDev } from "./util.js";
+import { pollResources } from "./resourceManager.js";
+import { getPreloadPath } from "./pathResolver.js"
 app.on("ready", () => {
-    const mainWindow = new BrowserWindow({})
+    const mainWindow = new BrowserWindow({
+        webPreferences: {
+            preload: getPreloadPath(),
+        },
+    });
 
     if (isDev()) {
-        mainWindow.loadURL("https://localhost:5123")
+        mainWindow.loadURL("http://localhost:5123/");
     } else {
-
-        mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"))
+        mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
     }
+    pollResources()
 
     mainWindow.webContents.openDevTools();
-})
-
-
-
-
-
-
-
-
-
-
-
+});
