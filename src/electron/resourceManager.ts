a21fea0ad462a0c86c051ea0652b0985f3aaf59a
teklu abayneh <tekluabayneh@gmail.com>
2025-06-17 15:11:23 +0300

    feat: enhance resourceManager logic

--- src/electron/resourceManager.ts
+++ src/electron/resourceManager.ts
@@ -1,53 +1,54 @@
-import os from "os"
-import OsUtils from "os-utils"
-import fs from "fs"
-import { BrowserWindow } from "electron"
-const POLLING_INTERVAl = 500
+import osUtils from 'os-utils';
+import os from 'os';
+import fs from 'fs';
+import { BrowserWindow } from 'electron';
+import { ipcWebContentsSend } from './util.js';
 
-export const pollResources = (mainwindow: BrowserWindow) => {
+const POLLING_INTERVAL = 500;
+
+export function pollResources(mainWindow: BrowserWindow) {
     setInterval(async () => {
-        const cpuUsage = await getCpuUsage()
-        const ramUsage = getRamUsage()
-        const getStorage = getStorageData().useage
-        mainwindow.webContents.send("statstics", { cpuUsage, ramUsage, getStorage })
-    }, POLLING_INTERVAl)
-
+        const cpuUsage = await getCpuUsage();
+        const ramUsage = getRamUsage();
+        const storageData = getStorageData();
+        ipcWebContentsSend('statistics', mainWindow.webContents, {
+            cpuUsage,
+            ramUsage,
+            storageUsage: storageData.usage,
+        });
+    }, POLLING_INTERVAL);
 }
 
-const getCpuUsage = () => {
-    return new Promise((res) => {
-        OsUtils.cpuUsage(res)
-    })
-}
-
-
-const getRamUsage = () => {
-    return 1 - OsUtils.freememPercentage()
-}
-
-
-export const getStaticData = () => {
-    const totalStorage = getStorageData().total
-    const cpuModel = os.cpus()[0].model
-    const totalMemoryGB = Math.floor(OsUtils.totalmem() / 1024)
+export function getStaticData() {
+    const totalStorage = getStorageData().total;
+    const cpuModel = os.cpus()[0].model;
+    const totalMemoryGB = Math.floor(osUtils.totalmem() / 1024);
 
     return {
-
-
         totalStorage,
         cpuModel,
-        totalMemoryGB
-    }
-
+        totalMemoryGB,
+    };
 }
 
-const getStorageData = () => {
-    const stats = fs.statfsSync(process.platform === "win32" ? "C://" : "/")
-    const total = stats.bsize * stats.blocks
-    const free = stats.bfree * stats.bfree
+function getCpuUsage(): Promise<number> {
+    return new Promise((resolve) => {
+        osUtils.cpuUsage(resolve);
+    });
+}
+
+function getRamUsage() {
+    return 1 - osUtils.freememPercentage();
+}
+
+function getStorageData() {
+    // requires node 18
+    const stats = fs.statfsSync(process.platform === 'win32' ? 'C://' : '/');
+    const total = stats.bsize * stats.blocks;
+    const free = stats.bsize * stats.bfree;
 
     return {
         total: Math.floor(total / 1_000_000_000),
-        useage: 1 - free / total
-    }
+        usage: 1 - free / total,
+    };
 }
